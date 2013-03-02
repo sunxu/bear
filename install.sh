bfe6b9f217b6994d9a42f96620786f917283c1d7
sunxu <sunxu.name@gmail.com>
2013-03-02 16:29:48 +0800

    add bashrc and install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# get path of this file
+file_path="`dirname \`pwd\`'/'$BASH_SOURCE`"
+
+# add bear bashrc to home bashrc
+echo "source ${file_path}/conf/bashrc" >> ~/.bashrc
+
