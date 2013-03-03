9a1fd35e3873396faf8e5233b59458556cfdbdad
sunxu <sunxu.name@gmail.com>
2013-03-03 19:50:15 +0800

    add vimrc

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
 # get path of this file
-file_path="`dirname \`pwd\`'/'$BASH_SOURCE`"
+home="`dirname \`pwd\`'/'$BASH_SOURCE`"
 
 # add bear bashrc to home bashrc
-echo "source ${file_path}/conf/bashrc" >> ~/.bashrc
+echo "add bash rc ..."
+echo "source ${home}/conf/bashrc" >> ~/.bashrc
+
+# add bear vimrc to home vimrc
+echo "add vim rc ..."
+echo "set runtimepath+=${home}/conf/vim" >> ~/.vimrc
+echo "source ${home}/conf/vimrc" >> ~/.vimrc
 
 # update bashrc
+echo "update bash rc ..."
 source ~/.bashrc
+
+echo "install finish"
