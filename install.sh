39cb5887b4719a9556c6feeb4ef54aba10c7424c
sunxu <sunxu.name@gmail.com>
2013-12-27 19:57:28 +0800

    add git config

--- install.sh
+++ install.sh
@@ -12,8 +12,14 @@ echo "add vim rc ..."
 echo "set runtimepath+=${home}/conf/vim" >> ~/.vimrc
 echo "source ${home}/conf/vimrc" >> ~/.vimrc
 
+# add git config to home gitconfig
+echo "add git config ..."
+echo "[include]" >> ~/.gitconfig
+echo "    path = ${home}/conf/gitconfig" >> ~/.gitconfig
+
 # update bashrc
 echo "update bash rc ..."
 source ~/.bashrc
 
 echo "install finish"
+
